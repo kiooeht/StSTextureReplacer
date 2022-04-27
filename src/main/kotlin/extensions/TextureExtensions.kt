201835b005c0f9d60f4da97163f743ffcadf9ea8
Kiooeht <kiooeht@gmail.com>
2022-04-26 23:07:14 -0700

    Fix replaced atlas region names ending with 0

--- src/main/kotlin/extensions/TextureExtensions.kt
+++ src/main/kotlin/extensions/TextureExtensions.kt
@@ -5,3 +5,4 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas
 
 fun Texture.asAtlasRegion(): TextureAtlas.AtlasRegion =
     TextureAtlas.AtlasRegion(this, 0, 0, this.width, this.height)
+        .apply { index = -1 }
